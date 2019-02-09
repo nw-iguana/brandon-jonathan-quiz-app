3dcf005756f5ff2b321a4fec62c1eb48515b1d72
Jonathan Espinosa <jonathan.espinosa@gmail.com>
2019-02-09 12:28:20 -0800

    generateQuestion and renderQuestion functions

--- index.js
+++ index.js
@@ -1,19 +1,103 @@
-// User starts the quiz by clicking button
-// declare global variables for score & question counter
+// Questions
+
+const questions = [
+    {
+        question: "What does Command+Option+H do?",
+        answers:[ 
+            "Hides all windows except Finder's windows", 
+            "Hides the Finder window toolbar", 
+            "Opens the Spotlight window",
+            "Closes the active window"
+        ],
+        correctAnswer: "Hides all windows except Finder's windows",
+        icon: "",
+        alt: ""
+    },
+    {
+        question: "How do you close your current window?",
+        answers:[
+            "Command+W",
+            "Command+Option+Space",
+            "Command+Option+H",
+            "Command+Shift+U"
+        ],
+        correctAnswer: "Command+W",
+        icon: "",
+        alt: ""
+    },
+    {
+        question: "What does Command+M do?",
+        answers: [
+            "Displays the Connect to Serve dialog",
+            "Minimizes the active window",
+            "Ejects the selected volume",
+            "Duplicates the selected items"
+        ],
+        correctAnswer: "Minimizes the active window",
+        icon: "",
+        alt: ""
+    }
+
+// 4. How do you select all the items in the active window?
+// a. Command+A (Correct)
+// b. Command+I
+// c. Command+H
+// d. Command+Alt+T
+
+];
+
+const STORE = {
+    state: 'startPage',
+    numberOfQuestions: questions.length,
+    currentQuestion: 0,
+    correct: 0,
+    incorrect: 0
+}
 
 function startQuiz() {
     //when start button is clicked, hide intro div, displays question/answer
+    $('.startButton').on('click', function(event) {
+        console.log('startQuiz runs');
+        //$('.startPage').hide();
+        renderQuestion();
+    });
 }
+
+//startQuiz();
 
 // First question is delivered
 
 function renderQuestion() {
-    //pulling question from store
+    //pulling question from questions
+    let question = questions[STORE.currentQuestion].question;
+    let answers = questions[STORE.currentQuestion].answers;
+    let correctAnswer = questions[STORE.currentQuestion].correctAnswer;
+    let correct = STORE.correct;
+    let incorrect = STORE.incorrect;
+    let html = generateQuestion(question, answers);
     //add to HTML
+    $('.startPage').html(html);
 }
 
-function generateQuestion() {
-
+function generateQuestion(question, answers) {
+    return `<section role="article" id="question_Page">
+            <h2 class="question">
+                ${question}
+            </h2>
+            <form>
+                <fieldset> 
+                    <input type="radio" value="" name="answer1">
+                    <label class="">${answers[0]}</label><br>
+                    <input type="radio" value="" name="answer2">
+                    <label class="">${answers[1]}</label><br>
+                    <input type="radio" value="" name="answer3">
+                    <label class="">${answers[2]}</label><br>
+                    <input type="radio" value="" name="answer4"
+                    <label class="">${answers[3]}</label><br>
+                    <button class="">Submit</button>
+                </fieldset> 
+            </form>
+        </section>`
 }
 
 // User sees which question they're on
@@ -72,13 +156,17 @@ function restartQuiz() {
 
 function runQuiz() {
     //insert functions to start quiz
-    //startQuiz();
+    startQuiz();
     //renderQuestion(); etc.
 }
 
 $(runQuiz);
 
-<section role="article" id="question_Page">
+/* <section role="article" id="question_Page">
+    <h2 class="question">
+        Bacon ipsum dolor amet turkey ham kielbasa 
+        swine tail. Flank pork belly tongue boudin. 
+    </h2>
     <form>
         <fieldset> 
             <label class=""></label>
@@ -96,4 +184,4 @@ $(runQuiz);
 
 <button type="submit" class="">Next Question</button>
 
-<button type="submit">Restart Button</button>
+<button type="submit">Restart Button</button> */
