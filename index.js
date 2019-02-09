fc61e28002eb70363e776502120d02a284c4500f
Brandon El Haj <brandon.elhaj@gmail.com>
2019-02-08 18:41:50 -0800

    Latest Version

---
+++ index.js
@@ -0,0 +1,19 @@
+<section role="article" id="question_Page">
+    <form>
+        <fieldset> 
+            <label class=""></label>
+            <input type="radio" value="" name="answer1">
+            <label class=""></label><br>
+            <input type="radio" value="" name="answer2">
+            <label class=""></label><br>
+            <input type="radio" value="" name="answer3">
+            <label class=""></label><br>
+            <input type="radio" value="" name="answer4"><br>
+            <button class="">Submit</button>
+        </fieldset> 
+    </form>
+</section>
+
+<button type="submit" class="">Next Question</button>
+
+<button type="submit">Restart Button</button>
