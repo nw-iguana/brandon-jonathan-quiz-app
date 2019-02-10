b55128ac98c81877a16198ed1f1137669932c02c
Brandon El Haj <brandon.elhaj@gmail.com>
2019-02-10 12:10:10 -0800

    latest changes

---
+++ STORE.js
@@ -0,0 +1,47 @@
+// Questions
+
+const STORE = [
+    {
+        question: "What does Command+Option+H do?",
+        answers:[ 
+            "Hides all windows except Finder's windows", 
+            "Hides the Finder window toolbar", 
+            "Opens the Spotlight window",
+            "Closes the active window"
+        ],
+        correctAnswer: "Hides all windows except Finder's windows",
+        icon: "",
+        alt: ""
+    },
+    {
+        question: "How do you close your current window?",
+        answers:[
+            "Command+W",
+            "Command+Option+Space",
+            "Command+Option+H",
+            "Command+Shift+U"
+        ],
+        correctAnswer: "Command+W",
+        icon: "",
+        alt: ""
+    },
+    {
+        question: "What does Command+M do?",
+        answers: [
+            "Displays the Connect to Serve dialog",
+            "Minimizes the active window",
+            "Ejects the selected volume",
+            "Duplicates the selected items"
+        ],
+        correctAnswer: "Minimizes the active window",
+        icon: "",
+        alt: ""
+    }
+
+// 4. How do you select all the items in the active window?
+// a. Command+A (Correct)
+// b. Command+I
+// c. Command+H
+// d. Command+Alt+T
+
+];
